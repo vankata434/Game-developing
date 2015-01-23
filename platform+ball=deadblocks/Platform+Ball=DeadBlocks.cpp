f1d4434fd20dc88c14c0b9a15136b7a10caaa443
vankata434 <todorovp@uni-sofia.bg>
2015-01-23 05:47:49 +0200

    Losing + Sound + Menu

--- platform+ball=deadblocks/Platform+Ball=DeadBlocks.cpp
+++ platform+ball=deadblocks/Platform+Ball=DeadBlocks.cpp
@@ -2,12 +2,17 @@
 #include <vector>
 #include <conio.h>
 #include <time.h>
+#include <string>
+#include <fstream>
 
 #include "ConsoleGaming.h"
+
 
 using namespace std;
 
 HANDLE consoleHandle;
+bool escHit = 0;
+bool lose = 0;
 
 typedef vector<GameObject>::iterator randomAccess_iterator;
 typedef vector<GameObject>::const_iterator const_iterator;
@@ -17,30 +22,33 @@ const int WindowWidth = 70;
 const int WindowHeight = 30;
 // Block variables
 const char BlockSymbol = '-';
+const char hitBlockSymbol = ' ';
 int blockSpeed = 1;
 
+//09.01.2015 02:35 changed ball initial direction
 // Ball variables
-int ballSpeed = 1;
+int ballSpeed = -1;//skorostta po y zadyljitelno e +/-1 kogato nablijava blokovete, za da ne gi preska4a
+int ballSpeedX = 1;
 
 // Platform variables
-int platformSpeed = 1;
-int platformLength = 5;
+int platformSpeed = 3;
+int platformLength = 10;
 
 // Game variables
-unsigned long sleepDuration = 100;
+unsigned long sleepDuration = 50;
 
 GameObject ball(WindowWidth / 2, WindowHeight / 2, '*');
 vector<GameObject> blocks;
 vector<GameObject> platform;
 
-unsigned int blockSpawnInterval = 10;
+unsigned int blockSpawnInterval = 10;//taq promenliva nikade ne se polzva; qvno trqbva da q polzwame za ne6to
 
 void Update()
 {
 	COORD direction = { 0, 0 };
-	if (kbhit())
+	if (_kbhit())
 	{
-		char key = getch();
+		char key = _getch();
 		switch (key)
 		{
 		case 'a':
@@ -50,29 +58,46 @@ void Update()
 		case 'd':
 			//direction.X = ballSpeed;
 			direction.X = 1;
-			break;;
+			break;
+		case 27:
+			escHit = 1;
+			break;
 		};
 	}
+	//09.01.2015 02:13 ograni4ena e platformata da ne izliza izvan ekrana
 	int beginOfPlatform = platform.begin()._Ptr->Coordinates.X, endOfPlatform = beginOfPlatform + platform.size() - 1;
 	for (randomAccess_iterator platformBody = platform.begin(); platformBody != platform.end(); ++platformBody)
 	{
-
 		if ((beginOfPlatform > 0 || direction.X>0) && (endOfPlatform < 68 || direction.X<0))
-
-
 		{
-			platformBody->Coordinates.X += direction.X;
+			platformBody->Coordinates.X += direction.X*platformSpeed;
+			platformBody->Coordinates.Y += direction.Y*platformSpeed;
 
-			platformBody->Coordinates.Y += direction.Y;
 		}
 	}
-
 	ball.Coordinates.Y += ballSpeed;
+	ball.Coordinates.X += ballSpeedX;
+	if (ball.Coordinates.X <= 0)
+	{
+		ballSpeedX *= -1;
+		cout << '\a';
+	}
+	else if (ball.Coordinates.X >= 68)
+	{
+		ballSpeedX *= -1;
+		cout << '\a';
+	}
+	if ((ball.Coordinates.Y >= WindowHeight - 1) &&
+		(ball.Coordinates.X < beginOfPlatform || ball.Coordinates.X > endOfPlatform)) lose = 1; //gubi6; sled nego ne trqbwa li da ima i break?
 	if (ball.Coordinates.Y >= WindowHeight - 1 || ball.Coordinates.Y <= 0)
 	{
+		cout << '\a';
 		ballSpeed = -ballSpeed;
+		if (direction.X > 0) ballSpeedX += 1;
+		else if (direction.X < 0) ballSpeedX -= 1;//kogato mardame nalqvo/nadqsno platformata i top4eto da se marda
 	}
 
+	//09.01.2015 02:51 dobaveno e otnemane na ucelen blok
 	// Loop trough all blocks
 	for (randomAccess_iterator block = blocks.begin(); block != blocks.end(); ++block)
 	{
@@ -92,12 +117,12 @@ void Update()
 				break;
 			}
 		}
-
 	}
 }
 
 void Draw()
 {
+	//miganeto na ekrana ako moje da se mahne...?
 	ClearScreen(consoleHandle);
 
 	for (const_iterator platformBody = platform.cbegin(); platformBody != platform.cend(); ++platformBody)
@@ -116,39 +141,97 @@ void Draw()
 
 int main()
 {
+
+	PlaySound(TEXT("Miley.wav"), NULL, SND_FILENAME | SND_LOOP | SND_ASYNC);//da se dobavi i sila na zvuka
+
 	//golemina na ekrana
 	system("MODE 71,30");//ne pipai 6iro4inata na ekrana, ako e po-tesen se poqvqva byg
+	//menu
+	string option = "";
 
-	consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
-
-	srand(time(NULL));
-
-	int platformY = WindowHeight - 1;
-	int platformX = WindowWidth / 2 - platformLength / 2;
-	char platformSymbol = '-';
-
-	for (int i = 0; i < platformLength; i++)
+	while (1)
 	{
-		platform.push_back(GameObject(platformX + i, platformY, platformSymbol));
-	}
-
-
-	// Generate blocks
-	int blocksPerColumn = 3;
-	for (int i = 0; i < WindowWidth - 1; i++)
-	{
-		for (int j = 0; j < blocksPerColumn; j++)
+		escHit = 0;
+		lose = 0;
+		consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+		ClearScreen(consoleHandle);
+		cout << "Home screen(Esc)" << endl;
+		cout << "Select option" << endl << endl;
+		cout << "Start(s)" << endl;
+		cout << "Instructions(i)" << endl;//not ready
+		cout << "High scores(h)" << endl;
+		cout << "Change settings(c)" << endl;//not ready
+		cout << "Exit(x)" << endl;
+		cin >> option;
+		if (option == "s")
 		{
-			blocks.push_back(GameObject(i, j, BlockSymbol));
+			consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+			srand(time(NULL));
+
+			int platformY = WindowHeight - 1;
+			int platformX = WindowWidth / 2 - platformLength / 2;
+			//09.01.2015 02:55 changed platform symbol
+			char platformSymbol = '_';
+
+			for (int i = 0; i < platformLength; i++)
+			{
+				platform.push_back(GameObject(platformX + i, platformY, platformSymbol));
+			}
+
+
+			// Generate blocks
+			int blocksPerColumn = 3;
+			for (int i = 0; i < WindowWidth - 1; i++)
+			{
+				for (int j = 0; j < blocksPerColumn; j++)
+				{
+					blocks.push_back(GameObject(i, j, BlockSymbol));
+				}
+			}
+
+			while (true)
+			{
+
+				if (escHit) break;
+				else if (lose)
+				{
+					consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+					ClearScreen(consoleHandle);
+					cout << "LOSER" << endl;
+					cout << "Press Esc for home screen" << endl;
+					for (int i = 0; i <= 6; i++) cout << '\a';
+					//while da ne se maha, kakto i skobite pod nego
+					while (_getch() != 27)
+					{
+					}
+					WinExec("platform+Ball=DeadBlocks", SW_SHOW);//ako moje pesenta da ne po4va otna4alo tuk...
+					exit(1);
+
+				}
+				else
+				{
+					Update();
+					Draw();
+					Sleep(sleepDuration);
+				}
+			}
+		}
+		else if (option == "h")
+		{
+			consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+			ClearScreen(consoleHandle);
+			long score;
+			string name;
+			ifstream score_file("score.txt");
+			if (score_file)   score_file >> name >> score;
+			cout << name << " " << score << endl;
+			cout << "Press any key for home screen" << endl;
+			_getch();
+		}
+		else if (option == "x")
+		{
+			return 0;
 		}
 	}
-
-	while (true)
-	{
-		Update();
-		Draw();
-		Sleep(sleepDuration);
-	}
-
-	return 0;
 }
